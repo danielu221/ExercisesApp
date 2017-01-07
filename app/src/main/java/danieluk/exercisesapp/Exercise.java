fccfb689741a9b80c70a882c3a9d3357dc4fe829
danielu221 <danielukmateusz@gmail.com>
2017-01-07 17:21:24 +0100

    Added attributes

--- app/src/main/java/danieluk/exercisesapp/Exercise.java
+++ app/src/main/java/danieluk/exercisesapp/Exercise.java
@@ -9,18 +9,20 @@ public class Exercise {
     /**
      * Created by Danielu on 2017-01-03.
      */
+    int id;
     String name;
     int series;
+    int reps;
+    int weights;
+    String notes;
 
-    public int getSeries() {
-        return series;
+    public int getId() {
+        return id;
     }
 
-    public void setSeries(int series) {
-        this.series = series;
+    public void setId(int id) {
+        this.id = id;
     }
-
-
 
     public void setName(String name) {
         this.name = name;
@@ -30,10 +32,37 @@ public class Exercise {
         return name;
     }
 
+    public int getSeries() {
+        return series;
+    }
 
+    public void setSeries(int series) {
+        this.series = series;
+    }
 
+    public int getReps() {
+        return reps;
+    }
 
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
 
+    public int getWeights() {
+        return weights;
+    }
+
+    public void setWeights(int weights) {
+        this.weights = weights;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 
 
 
